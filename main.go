dc937b24a3be0dc49ee5e2693228dcb9008e9a47
mi759 <68837796+mi759@users.noreply.github.com>
2025-04-25 13:20:54 +0000

    add database connection sample code

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading .env file: %v\n", err)
+	}
+
+	fmt.Println(os.Getenv("DATABASE_URL"))
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var content string
+	err = conn.QueryRow(context.Background(), "select content from haiku where id=$1", 1).Scan(&content)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(content)
+
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
